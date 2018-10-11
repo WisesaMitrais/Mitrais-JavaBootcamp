9a1b2c4a7407dd0f42e4c2f00666cc2087e561bc
I Gede Wisesa Priya Fentika <Wisesa_F1847@mitrais.com>
2018-10-11 13:04:23 +0800

    add a model and create a bean to handle hacker rank problem

--- java-bootcamp/src/main/java/com/mitrais/javabootcamp/JavaBootcampApplication.java
+++ java-bootcamp/src/main/java/com/mitrais/javabootcamp/JavaBootcampApplication.java
@@ -1,12 +1,29 @@
 package com.mitrais.javabootcamp;
 
+import model.HackerRankProblem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JavaBootcampApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaBootcampApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(JavaBootcampApplication.class, args);
+		System.out.println("Java-Bootcamp running...");
+
+		HackerRankProblem hrp = applicationContext.getBean("HackerRankProblemBean", HackerRankProblem.class);
+		System.out.println(hrp.getProblemID());
+	}
+
+	@Bean(name = "HackerRankProblemBean")
+	public HackerRankProblem setHackerRankProblem(){
+		HackerRankProblem hrp = new HackerRankProblem(
+				11301,
+				"Algorithm",
+				"Warmup",
+				"WU_AVeryBigSum");
+		return hrp;
 	}
 }
