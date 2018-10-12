fd0423b3662338e37df1acb1d5987d9a95071022
I Gede Wisesa Priya Fentika <Wisesa_F1847@mitrais.com>
2018-10-12 15:14:29 +0800

    Java basic keypoint 1, 2, 3, 4, 5, 6, 7, 8, 10

--- JavaBasic/src/hackerrank/DummyData.java
+++ JavaBasic/src/hackerrank/DummyData.java
@@ -2,15 +2,51 @@ package hackerrank;
 
 public class DummyData {
     
-    public static final int[] PROBLEM_ID = {
+    private static final int[] PROBLEM_ID = {
         11301, 11302, 11303, 11304, 11305, 11306, 11307, 11308, 11309, 11310};
-    public static final String[] PROBLEM_CATEGORY = {
+    private static final String[] PROBLEM_CATEGORY = {
         "Algorithm"};
-    public static final String[] PROBLEM_TYPE = {
+    private static final String[] PROBLEM_TYPE = {
         "Warmup"};
-    public static final String[] PDF_NAME = {
+    private static final String[] PDF_NAME = {
         "WU_AVeryBigSum", "WU_BirthdayCakeCandles", "WU_CompareTheTriplets", 
         "WU_DiagonalDifference", "WU_MiniMaxSum", "WU_PlusMinus", 
         "WU_SimpleArraySum", "WU_SolveMeFirst", "WU_Staircase", 
         "WU_TimeConversion"};
+    
+    public int[] getProblemId(){
+        return PROBLEM_ID;
+    }
+    
+    public int getOneProblemId(int index){
+        if(index > 9 || index < 0) return -1;
+        else return PROBLEM_ID[index];
+    }
+    
+    public String[] getProblemCategory(){
+        return PROBLEM_CATEGORY;
+    }
+    
+    public String getOneProblemCategory(int index){
+        if(index != 0) return "Not Found";
+        else return PROBLEM_CATEGORY[index];
+    }
+    
+    public String[] getProblemType(){
+        return PROBLEM_TYPE;
+    }
+    
+    public String getOneProblemType(int index){
+        if(index != 0) return "Not Found";
+        else return PROBLEM_TYPE[index];
+    }
+    
+    public String[] getPDFName(){
+        return PDF_NAME;
+    }
+    
+    public String getOnePDFName(int index){
+        if(index > 9 || index < 0) return "Not Found";
+        else return PDF_NAME[index];
+    }
 }
--- JavaBasic/src/javabasic/JavaBasic.java
+++ JavaBasic/src/javabasic/JavaBasic.java
@@ -1,8 +1,20 @@
 package javabasic;
 
+import hackerrank.HackerRankProblem;
+import hackerrank.DummyData;
+
 public class JavaBasic {
 
     public static void main(String[] args) {
         System.out.println("Java Basic Concept");
+        
+        DummyData dummyData = new DummyData();
+        HackerRankProblem hackerRankProblem = new HackerRankProblem(
+                dummyData.getOneProblemId(0), dummyData.getOneProblemCategory(0),
+                dummyData.getOneProblemType(0), dummyData.getOnePDFName(0));
+        System.out.println(hackerRankProblem.getProblemID());
+        System.out.println(hackerRankProblem.getProblemCategory());
+        System.out.println(hackerRankProblem.getProblemType());
+        System.out.println(hackerRankProblem.getPdfName());
     }
 }
