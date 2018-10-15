65d99f25549519ecaa6bb6d6b70b5b3318e68871
I Gede Wisesa Priya Fentika <Wisesa_F1847@mitrais.com>
2018-10-15 08:58:31 +0800

    Java basic keypoints 9

--- JavaBasic/src/javabasic/JavaBasic.java
+++ JavaBasic/src/javabasic/JavaBasic.java
@@ -9,12 +9,21 @@ public class JavaBasic {
         System.out.println("Java Basic Concept");
         
         DummyData dummyData = new DummyData();
-        HackerRankProblem hackerRankProblem = new HackerRankProblem(
-                dummyData.getOneProblemId(0), dummyData.getOneProblemCategory(0),
-                dummyData.getOneProblemType(0), dummyData.getOnePDFName(0));
-        System.out.println(hackerRankProblem.getProblemID());
-        System.out.println(hackerRankProblem.getProblemCategory());
-        System.out.println(hackerRankProblem.getProblemType());
-        System.out.println(hackerRankProblem.getPdfName());
+        HackerRankProblem[] hackerRankProblemList = new HackerRankProblem[10];
+        //Insert dummy data to array.
+        for(int index = 0; index <= 9; index++){
+            hackerRankProblemList[index] = new HackerRankProblem(
+            dummyData.getOneProblemId(index), dummyData.getOneProblemCategory(0),
+                dummyData.getOneProblemType(0), dummyData.getOnePDFName(index));
+        }
+        //Print array data.
+        int index = 0;
+        do{
+            System.out.println("Problem-ID: "+hackerRankProblemList[index].getProblemID()+
+                    ". Problem-Category: "+hackerRankProblemList[index].getProblemCategory()+
+                    ". Problem-Type: "+hackerRankProblemList[index].getProblemType()+
+                    ". PDF-Name: "+hackerRankProblemList[index].getPdfName());
+            index++;
+        }while(index < 10);
     }
 }
